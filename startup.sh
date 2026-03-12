f2c349e42264a1ce6b6466a5f6bee7e1b38ce2f3
Abhinav Shukla <abhinav.sh226@gmail.com>
2026-03-12 23:13:25 +0530

    renamed folders

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ cd Backend
 pip install -r requirements.txt
 
 # start FastAPI app
-gunicorn --bind=0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker Backend.app:app
+gunicorn --bind=0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker backend.app:app
