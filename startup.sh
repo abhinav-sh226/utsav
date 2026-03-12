e5453a503f39cd7aea586b38b2b60fdc98c9a7ee
Abhinav Shukla <abhinav.sh226@gmail.com>
2026-03-12 23:28:57 +0530

    startup.sh updated

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# move into Backend folder
+# move into backend folder
 cd backend
 
 # install dependencies
 pip install -r requirements.txt
 
 # start FastAPI app
-gunicorn --bind=0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker backend.app:app
+gunicorn --bind=0.0.0.0:$PORT \
+--worker-class uvicorn.workers.UvicornWorker \
+app:app
